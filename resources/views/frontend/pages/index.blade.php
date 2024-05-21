7838ceeb1c9700db8db1b8f6a88c05e1cbe9f6d2
alptrkslmn <alptarik.salman@gmail.com>
2024-05-21 19:00:19 +0300

    Composer Güncelleme

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -55,35 +55,35 @@
       <div class="container">
         <div class="row">
           <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
-            <a class="block-2-item" href="#">
+            <a class="block-2-item" href="{{route('kadinurunler')}}">
               <figure class="image">
                 <img src="images/women.jpg" alt="" class="img-fluid">
               </figure>
               <div class="text">
-                <span class="text-uppercase">Collections</span>
+                <span class="text-uppercase">Giyim</span>
                 <h3>Kadın</h3>
               </div>
             </a>
           </div>
           <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
-            <a class="block-2-item" href="#">
+            <a class="block-2-item" href="{{route('cocukurunler')}}">
               <figure class="image">
                 <img src="images/children.jpg" alt="" class="img-fluid">
               </figure>
               <div class="text">
-                <span class="text-uppercase">Collections</span>
-                <h3>Children</h3>
+                <span class="text-uppercase">Giyim</span>
+                <h3>Çocuk</h3>
               </div>
             </a>
           </div>
           <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
-            <a class="block-2-item" href="#">
+            <a class="block-2-item" href="{{route('erkekurunler')}}">
               <figure class="image">
                 <img src="images/men.jpg" alt="" class="img-fluid">
               </figure>
               <div class="text">
-                <span class="text-uppercase">Collections</span>
-                <h3>Men</h3>
+                <span class="text-uppercase">Giyim</span>
+                <h3>Erkek</h3>
               </div>
             </a>
           </div>
@@ -95,7 +95,7 @@
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-md-7 site-section-heading text-center pt-4">
-            <h2>Featured Products</h2>
+            <h2>Öne Çıkan Ürünler</h2>
           </div>
         </div>
         <div class="row">
@@ -171,7 +171,7 @@
       <div class="container">
         <div class="row justify-content-center  mb-5">
           <div class="col-md-7 site-section-heading text-center pt-4">
-            <h2>Big Sale!</h2>
+            <h2>Kampanya!</h2>
           </div>
         </div>
         <div class="row align-items-center">
@@ -179,7 +179,7 @@
             <a href="#"><img src="images/blog_1.jpg" alt="Image placeholder" class="img-fluid rounded"></a>
           </div>
           <div class="col-md-12 col-lg-5 text-center pl-md-5">
-            <h2><a href="#">50% less in all items</a></h2>
+            <h2><a href="#">Tüm ürünlerimizde 50%'ye varan indirimler'</a></h2>
             <p class="post-meta mb-4">By <a href="#">Carl Smith</a> <span class="block-8-sep">&bullet;</span> September 3, 2018</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam iste dolor accusantium facere corporis ipsum animi deleniti fugiat. Ex, veniam?</p>
             <p><a href="#" class="btn btn-primary btn-sm">Shop Now</a></p>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Frontend\PageHomeController;
 Route::get('/', [PageHomeController::class, 'anasayfa'])->name('anasayfa');
 
 Route::get('/urunler', [PageController::class, 'urunler'])->name('urunler');
+Route::get('/erkek-giyim', [PageController::class, 'urunler'])->name('erkekurunler');
+Route::get('/kadin-giyim', [PageController::class, 'urunler'])->name('kadinurunler');
+Route::get('/cocuk-giyim', [PageController::class, 'urunler'])->name('cocukurunler');
 
 Route::get('/urun/detay', [PageController::class, 'urundetay'])->name('urundetay');
 
