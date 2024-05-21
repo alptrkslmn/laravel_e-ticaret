a3c40486c84a1b043253a8583e2b510e159a8d4a
alptrkslmn <alptarik.salman@gmail.com>
2024-05-21 21:02:54 +0300

    4. Ders

--- app/Http/Controllers/Frontend/PageHomeController.php
+++ app/Http/Controllers/Frontend/PageHomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Slider;
@@ -11,6 +12,8 @@ class PageHomeController extends Controller
     public function anasayfa() {
         $slider = Slider::where('status','1')->first();
         $title = "Anasayfa";
-        return view('frontend.pages.index' ,compact('slider','title'));
+
+        $categories = Category::where('status', '1')->get();
+        return view('frontend.pages.index' ,compact('slider','title', 'categories'));
     }
 }
--- resources/views/frontend/inc/header.blade.php
+++ resources/views/frontend/inc/header.blade.php
@@ -42,17 +42,11 @@
                 <li class="has-children">
                     <a href="#">Kategori</a>
                         <ul class="dropdown">
-                            <li><a href="#">Menu One</a></li>
-                            <li><a href="#">Menu Two</a></li>
-                            <li><a href="#">Menu Three</a></li>
-                            <li class="has-children">
-                                <a href="#">Sub Menu</a>
-                                <ul class="dropdown">
-                                <li><a href="#">Menu One</a></li>
-                                <li><a href="#">Menu Two</a></li>
-                                <li><a href="#">Menu Three</a></li>
-                                </ul>
-                            </li>
+                            @if (!empty($categories) && $categories->count() > 0)
+                                @foreach ($categories as $category)
+                                <li><a href="#">{{$category->name}}</a></li>
+                                @endforeach
+                            @endif
                             </ul>
                         </li>
                         <li>
