1bf88816fb3e548d97d1499c16c2192e79b9df00
alptrkslmn <alptarik.salman@gmail.com>
2024-05-25 01:13:15 +0300

    12. Ders

--- app/Http/Controllers/Frontend/PageController.php
+++ app/Http/Controllers/Frontend/PageController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
 use App\Models\About;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PageController extends Controller
 {
-    public function urunler(){
-        $products = Product::where('status', '1')->paginate(1);
+    public function urunler(Request $request)
+    {
+
+        $size = $request->size ?? null;
+        $color = $request->color ?? null;
+        $startprice = $request->start_price ?? null;
+        $endprice = $request->end_price ?? null;
+        $products = Product::where('status', '1')
+            ->where(function ($q) use ($size, $color, $startprice, $endprice) {
+                if (!empty($size)) {
+                    $q->where('size', $size);
+                }
+                if (!empty($color)) {
+                    $q->where('color', $color);
+                }
+                if (!empty($startprice) && $endprice) {
+                    $q->whereBetween('price', [$startprice, $endprice]);
+                }
+                return $q;
+            })
+            ->paginate(1);
         return view('frontend.pages.products', compact('products'));
     }
     public function indirimdekiurunler(){
